86857604015dbf0d50d54b85e99a96387003da75
leiax00 <leiax00@outlook.com>
2020-06-02 15:45:59 +0800

    练习题提交

---
+++ src/java/wangzheng/dynamicProgramming/Dynamic03.java
@@ -0,0 +1,16 @@
+package wangzheng.dynamicProgramming;
+
+/**
+ * 有一个数字序列包含 n 个不同的数字，如何求出这个序列中的最长递增子序列长度？
+ * 比如 2, 9, 3, 6, 5, 1, 7 这样一组数字序列，它的最长递增子序列就是 2, 3, 5, 7，所以最长递增子序列的长度是 4。
+ */
+public class Dynamic03 {
+    public void solve(int[] serial) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] serial = {2, 9, 3, 6, 5, 1};
+        new Dynamic03().solve(serial);
+    }
+}
